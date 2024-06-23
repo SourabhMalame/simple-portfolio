23fb5428873b30065dcaebe9e6785515158ba5fb
Sourabh <sourabhmalame@gmail.com>
2024-06-23 18:34:49 +0530

    New Update In Ui

---
+++ src/Components/about/About.jsx
@@ -0,0 +1,38 @@
+// About.jsx
+
+import React from "react";
+import "./About.css"; // Import your CSS file
+import img3 from "../../../public/img3.svg";
+
+const About = () => {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src={img3} alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Outside of my professional endeavors, I have a diverse range of
+            interests that fuel my curiosity and creativity. Whether it's
+            delving into the latest developments in technology, exploring new
+            cultures through travel, or immersing myself in the arts, I
+            constantly seek out opportunities to broaden my horizons and expand
+            my perspective.
+          </p>
+          <p className="hero--section-description">
+            I believe in the power of personal development and lifelong
+            learning, and I am always eager to take on new challenges and push
+            myself to new heights. With a strong work ethic, a thirst for
+            knowledge, and a genuine enthusiasm for what I do, I am committed to
+            making a positive impact both personally and professionally.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/Components/hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Hero.css";
+import img from "../../../public/7118758_3323617.svg";
+const Hero = () => {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Sourabh Malame</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">
+              Full Stack Developer And Digital Marketer
+            </span>
+          </h1>
+          <p className="hero--section-description">
+            Combining my expertise as a seasoned full-stack developer with a
+            solid foundation in digital marketing, I possess a unique skill set
+            poised to drive innovative solutions and impactful digital
+            strategies in today's dynamic tech industry.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src={img} alt="Hero Section" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
